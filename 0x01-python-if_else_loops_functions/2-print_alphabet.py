f392edf2c141dc68a73e85339366a0b225573b9a
nyirenda95 <jonathannyirenda95@gmail.com>
2022-11-29 23:31:43 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+# 2-print_alphabet.py
 for letter in range(97, 123):
+    """Print the alphabet in lowercase, not followed by a new line."""
     print("{}".format(chr(letter)), end="")
