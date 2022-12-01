3d4a915ae5d59506d094fd8368911f29c253e3fc
nyirenda95 <jonathannyirenda95@gmail.com>
2022-12-01 11:56:46 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -26,3 +26,5 @@ if __name__ != "__main__":
                  exit(1)
 
                  result = func(int(argv[1]), int(argv[3]))
+                 print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+                         argv[2], int(argv[3]), result)
