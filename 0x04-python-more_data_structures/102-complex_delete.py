9ca871bb6b9aaf0a44ef4d14166d15c6ac62e5bb
nyirenda95 <jonathannyirenda95@gmail.com>
2022-12-08 11:24:15 -0800

    101-square_matrix_map.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-def complex_delete(a_dictionary, value):
-    for idx in list(a_dictionary.keys()):
-        if a_dictionary[idx] == value:
-            del a_dictionary[idx]
-            return a_dictionary
