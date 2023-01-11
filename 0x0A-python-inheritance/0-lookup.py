b5396972746d4596b67261c35c7647718a1843b5
nyirenda95 <jonathannyirenda95@gmail.com>
2023-01-11 02:16:52 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+"""
+=============================
+Module with the method lookup
+=============================
+"""
+
+
 def lookup(obj):
     """Function for return the attributes for an object"""
+
     return dir(obj)
